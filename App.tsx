43105a2f14206383ab08f92c4383115f5cf9b7a7
abhishektiwari <abhishek.iwdev@gmail.com>
2026-07-17 16:23:43 +0530

    remove lint error

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  const unusedVariable = "I am not used";
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
